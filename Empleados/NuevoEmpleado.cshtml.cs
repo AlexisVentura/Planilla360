e8bc44bad6a6c76cd6a88ca58d73e9af14f947ce
AlexisVentura <venturaalexis112@gmail.com>
2025-03-16 13:08:19 -0600

    Add files via upload

---
+++ Empleados/NuevoEmpleado.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Planilla360.Pages.Empleados
+{
+    public class NuevoEmpleadoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
