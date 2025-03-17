6d1ebdb44085aa7cfe6d613cbdc53fb33699bd6d
AlexisVentura <venturaalexis112@gmail.com>
2025-03-16 21:09:58 -0600

    Add files via upload

---
+++ Modelos/Empleado.cs
@@ -0,0 +1,16 @@
+﻿namespace Planilla360.Modelos
+{
+    public class Empleado
+    {
+        public int IdEmpleado { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string CorreoElectronico { get; set; }
+        public string Telefono { get; set; }
+        public string Puesto { get; set; }
+        public string SalarioBase { get; set; }
+        public string FechaIngreso { get; set; }
+        public string FechaNacimiento { get; set; }
+        public int IdPuesto { get; set; }
+    }
+}
